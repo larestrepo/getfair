df70acce26e4823b44280c0fc69d36c06cd188db
larestrepo <larestrepomu@gmail.com>
2022-05-19 13:36:24 +0000

    latest updates

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ import json
 from datetime import date
 import time
 from utils import read_query, write_query, kobo_api, ipfs
-import psycopg2
-
-import requests
-from requests.structures import CaseInsensitiveDict
 
 from psql import dblib
 
 """ Curl equivalent
-curl -X GET https://kf.kobotoolbox.org/api/v2/assets/auUF7gnnmorhZ7vSsMSozR/data.json -H 
+curl -X GET https://kf.kobotoolbox.org/api/v2/assets/auUF7gnnmorhZ7vSsMSozR/data.json -H
 "Authorization: Token f820e2c138e487e28c87fbb9af4685f7f68051a4"
 
 Usefull page:
@@ -32,7 +28,8 @@ if __name__ == '__main__':
     # TODO
     # 4. Handle response errors
     BASE_URL = "https://kf.kobotoolbox.org/api/v2/assets/"
-    ASSET_UID = "auUF7gnnmorhZ7vSsMSozR"
+    # ASSET_UID = "auUF7gnnmorhZ7vSsMSozR"
+    ASSET_UID = "aPjKshuFjP3kPMiykHHD4Z"
     URL = f'https://kf.kobotoolbox.org/api/v2/assets/{ASSET_UID}/data/'
 
     TODAY = date.fromtimestamp(time.time())
@@ -40,13 +37,13 @@ if __name__ == '__main__':
 
     QUERY = f'{{"_submission_time":{{"$gt":"{TODAY}"}}}}'
     params = {
-        'query': QUERY,
+        # 'query': QUERY,
         'format': 'json'
     }
     rawResult = kobo_api(URL, params)
     rawResult = json.loads(rawResult.content.decode('utf-8'))
 
-    with open('./Aguacate_project_filtered.json', 'w') as file:
+    with open('./Abastecimiento.json', 'w') as file:
         json.dump(rawResult, file, indent=4, ensure_ascii=False)
 
     # Get the Project ID Index
@@ -64,7 +61,7 @@ if __name__ == '__main__':
                 column_list = []
                 values = []
                 tableName = 'data'
-                # TODO: read this list from json which was used to create the table as well. 
+                # TODO: read this list from json which was used to create the table as well.
                 columns = [
                     'start',
                     'end',
@@ -88,17 +85,16 @@ if __name__ == '__main__':
                         if register[column]:
                             column_list.append(column)
                             # If it is a string, the value should go with ''
-                            if type(register[column]) ==str:
+                            if type(register[column]) == str:
                                 value = register[column].replace("'", "''")
-                                value = "'" +value + "'"
+                                value = "'" + value + "'"
                             else:
                                 value = register[column]
                             values += [str(value)]
 
                 # The source data comes with start and end but these are reserved words in postgres, so need to change to start_date and end_date
-                column_list[0]= 'start_date'
-                column_list[1]= 'end_date'
-                
+                column_list[0] = 'start_date'
+                column_list[1] = 'end_date'
                 query += "(project_id, " + ", ".join(column_list) + ")\nVALUES"
                 project_id_str = "'" + str(project_id) + "', "
                 query += "(" + project_id_str + ", ".join(values) + "), \n"
@@ -116,7 +112,6 @@ if __name__ == '__main__':
                     if MainImageName == attachment['filename'].split("/")[-1]:
                         instance = attachment['instance']
                         id = attachment['id']
-                
                 # Insert picture data in table
                 tableName = 'pictures'
                 columns = [
@@ -127,13 +122,7 @@ if __name__ == '__main__':
                     'name',
                     'url'
                 ]
-                values = (str(project_id),
-                    str(data_id),
-                    str(id),
-                    str(instance),
-                    str(MainImageName),
-                    str(download_url))
-                
+                values = (str(project_id), str(data_id), str(id), str(instance), str(MainImageName), str(download_url))
                 picture_id = dblib.insert_picture(tableName, columns, values)
                 if instance != 0 or id != 0:
                     download_url = f"{URL}{instance}/attachments/{id}/"
@@ -142,16 +131,14 @@ if __name__ == '__main__':
                     rawResultImage.raw.decode_content = True
                     file_path = './pictures/' + MainImageName
                     with open(file_path, 'wb') as file:
-                        for chunk in rawResultImage.iter_content(chunk_size=16*1024):
+                        for chunk in rawResultImage.iter_content(chunk_size=16 * 1024):
                             file.write(chunk)
                         print('Image sucessfully Downloaded: ', MainImageName)
                     # Upload picture in IPFS
                     ipfs_result = ipfs(file_path)
                     IPFS_HASH = ipfs_result['IpfsHash']
-                    
                     # Update table with IPFS Hash
                     query = f"UPDATE {tableName} SET ipfshash = '{IPFS_HASH}' WHERE index = {picture_id} RETURNING id"
                     data_id = write_query(query)
     except TypeError:
         print(f"No projects found with ID: {ASSET_UID}")
-
